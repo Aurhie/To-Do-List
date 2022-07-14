d59c1059f8b17751bee47b30ff5e5d209a470524
Aurora Marques <Auroras321@hotmail.com>
2022-07-14 12:15:22 -0300

    Initial commit.

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Componentes
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtomDeleteAllComponent } from './components/todo-buttom-delete-all/todo-buttom-delete-all.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//Paginas
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtomDeleteAllComponent,
+    TodoInputAddItensComponent,
+    TodoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
